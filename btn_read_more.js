a93dad8e82064f31f5d2199c4cfcbad036342b52
GlebTroyakov <glebtroyakov@gmail.com>
2023-02-10 10:19:12 +0300

    init

---
+++ btn_read_more.js
@@ -0,0 +1,23 @@
+function readMore() {
+    let btn_open_text = document.getElementById("btn_open_text");
+    let btn_close_text = document.getElementById("btn_close_text");
+    let about_us_text = document.getElementById("about_us_text");
+
+    if (btn_open_text.style.display !== "none") {
+        about_us_text.style.height = "100%";
+        btn_open_text.style.display = "none";
+        btn_close_text.style.display = "inline";
+    } else if (window.innerWidth <= 768) {
+        about_us_text.style.height = "118px";
+        btn_open_text.style.display = "inline";
+        btn_close_text.style.display = "none";
+    } else if (window.innerWidth <= 1200) {
+        btn_open_text.style.display = "inline";
+        about_us_text.style.height = "160px";
+        btn_close_text.style.display = "none";
+    } else {
+        btn_open_text.style.display = "inline";
+        about_us_text.style.height = "155px";
+        btn_close_text.style.display = "none";
+    }
+}
