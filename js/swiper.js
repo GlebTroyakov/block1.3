57fc85f43592150e480a26175b8fd2b82c8deb18
GlebTroyakov <glebtroyakov@gmail.com>
2023-03-04 23:00:10 +0300

    destroy swiper with width >= 768

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,32 @@
-let swiper = new Swiper(".mySwiper", {
-    spaceBetween: 16,
-    mousewhell: true,
-    keyboard: true,
-    pagination: { // показывает не количество картинок, а количество сайпов до конца
-        el: ".swiper-pagination",
-        clickable: true, // переход по клику на пагинатор
+// let swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 16,
+//     mousewhell: true,
+//     keyboard: true,
+//     pagination: { // показывает не количество картинок, а количество сайпов до конца
+//         el: ".swiper-pagination",
+//         clickable: true, // переход по клику на пагинатор
+//     },
+//     slidesPerView: 'auto', // количество слайдов на экране
+//     watchOverflow: true, // отключает свайпер, если в не нет надобности (количество слайдов)
+//     slidesPerGroup: 1, // количество пролистываемых слайдов за 1 свайп
+//     // freeMode: true, // свайп на произвольное место, а не на начало картинки
+//     // loop:true, // бесконечная карусель
+// });
+
+let swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
     },
-    slidesPerView: 'auto', // количество слайдов на экране
-    watchOverflow: true, // отключает свайпер, если в не нет надобности (количество слайдов)
-    slidesPerGroup: 1, // количество пролистываемых слайдов за 1 свайп
-    // freeMode: true, // свайп на произвольное место, а не на начало картинки
-    // loop:true, // бесконечная карусель
+    breakpoints: {
+        320: {
+            enabled: true,
+            slidesPerView: 'auto',
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 'auto',
+            enabled: false,
+        },
+    },
 });
