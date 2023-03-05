3491a68c43790ac6d8684b14bbe5d6364fe76faf
GlebTroyakov <glebtroyakov@gmail.com>
2023-03-05 09:32:14 +0300

    fixed height with btn-read-more

--- js/btn_read_more.js
+++ js/btn_read_more.js
@@ -8,12 +8,12 @@ function readMore() {
         btn_open_text.style.display = "none";
         btn_close_text.style.display = "inline";
     } else if (window.innerWidth <= 768) {
-        about_us_text.style.height = "118px";
+        about_us_text.style.height = "120px";
         btn_open_text.style.display = "inline";
         btn_close_text.style.display = "none";
     } else if (window.innerWidth <= 1200) {
         btn_open_text.style.display = "inline";
-        about_us_text.style.height = "160px";
+        about_us_text.style.height = "150px";
         btn_close_text.style.display = "none";
     } else {
         btn_open_text.style.display = "inline";
